f058ce596cc9d9a7950438495bc5d7134fe5d304
Dylan Santos de Pinho <dylan.santosdepinho@he-arc.ch>
2017-06-03 17:41:19 +0200

    Fin de game

--- commandebot.py
+++ commandebot.py
@@ -7,6 +7,7 @@ import string
 
 row = 9
 board_player = [" "]*row**2
+numbers_of_boats = 3
 player = {"id":0,"board":board_player,"nb_boat":0}
 # game = {"player1":player.copy,"player2":player.copy}
 games = {"player1":player.copy(),"player2":player.copy(), "turn":0}
@@ -45,11 +46,26 @@ def my_board(data):
         return [data['d']['author']['id'],board]
     else:
         return [data['d']['author']['id'],"Please start the game"]
+    
+def adversary_board(data):
+    if is_game_started:
+        player = get_other_player(data)
+        
+        if player  == "try again":
+            return [data['d']['author']['id'],"Mais t'es qui?"]
+        
+        board ="board de l'adversaire\n__``` |" +  "|".join(letter for letter in string.ascii_uppercase[0:row]) + "|\n"
+        for num in range(0,row,1):
+            board += str(num+1) + "".join("|"+games[player]["board"][num*row+i] if not games[player]["board"][num*row+i] == 'B' else '| ' for i in range(0,row)) + "|\n"
+        board += "```__"
+        return [data['d']['author']['id'],board]
+    else:
+        return [data['d']['author']['id'],"Please start the game"]
 
 def fire(data):
     if are_boats_placed:
+        global is_game_started
         if games["turn"] == data['d']['author']['id']:
-            
             player = get_other_player(data)
             
             if player  == "try again":
@@ -60,10 +76,14 @@ def fire(data):
             case_impact = (ord(case_coord[1])-49)*9+ord(case_coord[0])-65
             if games[player]['board'][case_impact] == 'B':
                 games[player]['board'][case_impact] = 'X'
+                games[player]['board']["nb_boat"] -= 1
+                if games[player]['board']["nb_boat"] == 0 :
+                    is_game_started = False
+                    return [data['d']['author']['id'],"You won the game",games[get_other_player(data)]['id'],"Your lost the game."]
             else:
                 games[player]['board'][case_impact] = 'O'
             games["turn"] = games[player]['id']
-            return adversary_board(data)
+            return [data['d']['author']['id'],adversary_board(data)[1],games[get_other_player(data)]['id'],"Your turn now."]
         else:
             return [data['d']['author']['id'],"It's not your turn.",get_other_player(data),"Your friend is waiting for you."]
     else:
@@ -79,13 +99,15 @@ def put(data):
         if player  == "try again":
             return [data['d']['author']['id'],"Mais t'es qui?"]
         
-        if games[player]['nb_boat'] == 5:
+        if games[player]['nb_boat'] == numbers_of_boats:
             return [data['d']['author']['id'],"All your boats are placed"]
         
         game_temp = games[player]['board'].copy()
         
         collision = False
         str = data['d']['content']
+        if len(shlex.split(str, posix=False))!=3:
+            return [data['d']['author']['id'],"Wrong syntax."]
         case_coord = shlex.split(str, posix=False)[1].upper()
         position =  shlex.split(str, posix=False)[2].upper()
         case_posage = (ord(case_coord[1])-49)*9+ord(case_coord[0])-65
@@ -111,9 +133,9 @@ def put(data):
         if not collision:
             games[player]['board'] = game_temp.copy()  
             games[player]["nb_boat"] +=1 
-            if games['player1']["nb_boat"]==5 and games['player2']["nb_boat"]==5:
+            if games['player1']["nb_boat"]==numbers_of_boats and games['player2']["nb_boat"]==numbers_of_boats:
                 are_boats_placed = True
-                return [games['player1'][id],"Your turn to fire",games['player2'],"Wait for the other player to fire."]
+                return [games['player1']['id'],"Your turn to fire",games['player2']['id'],"Wait for the other player to fire."]
             return my_board(data)        
         else:
             return [data['d']['author']['id'],"The way is obstructed."]
@@ -138,20 +160,5 @@ def get_other_player(data):
     else:
         return 'try again'
             
-
-def adversary_board(data):
-    if is_game_started:
-        player = get_other_player(data)
-        
-        if player  == "try again":
-            return [data['d']['author']['id'],"Mais t'es qui?"]
-        
-        board ="board de l'adversaire\n__``` |" +  "|".join(letter for letter in string.ascii_uppercase[0:row]) + "|\n"
-        for num in range(0,row,1):
-            board += str(num+1) + "".join("|"+games[player]["board"][num*row+i] if not games[player]["board"][num*row+i] == 'B' else '| ' for i in range(0,row)) + "|\n"
-        board += "```__"
-        return [data['d']['author']['id'],board]
-    else:
-        return [data['d']['author']['id'],"Please start the game"]
 
     
